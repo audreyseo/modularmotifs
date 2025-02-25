f851be121cfbd4aa22d21fd3bfedc76bf65e9669
Audrey Seo <audreyleeseo@gmail.com>
2025-02-24 21:36:57 -0800

    Add name to PixelGrid
    Added getters, setters, and existence checkers. Closes #62

--- modularmotifs/core/pixel_grid.py
+++ modularmotifs/core/pixel_grid.py
@@ -1,8 +1,21 @@
 import abc
+from typing import Optional, Self
 from modularmotifs.core.rgb_color import RGBColor
 
 
 class PixelGrid(abc.ABC):
+    _name: Optional[str] = None
+    
+    def set_name(self, name: str) -> Self:
+        self._name = name
+        pass
+    
+    def get_name(self) -> Optional[str]:
+        return self._name
+    
+    def has_name(self) -> bool:
+        return self.get_name() is not None
+    
     @abc.abstractmethod
     def width(self) -> int:
         pass
@@ -13,10 +26,6 @@ class PixelGrid(abc.ABC):
     
     def in_range(self, x: int, y: int) -> bool:
         return 0 <= x < self.width() and 0 <= y < self.height()
-    
-    # @abc.abstractmethod
-    # def get_color(self, x: int, y: int) -> int:
-    #     pass
     
     @abc.abstractmethod
     def get_rgb(self, x: int, y: int) -> RGBColor:
