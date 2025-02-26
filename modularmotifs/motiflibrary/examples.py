e94bdd05d08295a90f20db41cedc4d4bf259a80f
Jack Zhang <jzhang18@cs.washington.edu>
2025-02-26 13:04:31 -0800

    add bird motif pngs to examples.py

--- modularmotifs/motiflibrary/examples.py
+++ modularmotifs/motiflibrary/examples.py
@@ -1,5 +1,7 @@
 from modularmotifs.core.motif import Color, Motif
-
+from modularmotifs.core.util import png2motif
+from pathlib import Path
+PATH = Path(__file__).parent.absolute()
 
 def int_to_color(i: int) -> Color:
     assert (
@@ -31,17 +33,18 @@ motifs = {
     "plus-3x3": int_lol_to_motif([[2, 1, 2], [1, 1, 1], [2, 1, 2]]),
     "x-3x3": int_lol_to_motif([[1, 2, 1], [2, 1, 2], [1, 2, 1]]),
     "crosshair-3x3": int_lol_to_motif([[2, 1, 2], [1, 2, 1], [2, 1, 2]]),
-    "triangle1-5x9": int_lol_to_motif(
-        [
-            [3, 3, 3, 3, 1, 3, 3, 3, 3],
-            [3, 3, 3, 1, 1, 1, 3, 3, 3],
-            [3, 3, 1, 1, 3, 1, 1, 3, 3],
-            [3, 1, 1, 3, 1, 3, 1, 1, 3],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1],
-        ]
-    ),
+    # "triangle1-5x9": int_lol_to_motif(
+    #     [
+    #         [3, 3, 3, 3, 1, 3, 3, 3, 3],
+    #         [3, 3, 3, 1, 1, 1, 3, 3, 3],
+    #         [3, 3, 1, 1, 3, 1, 1, 3, 3],
+    #         [3, 1, 1, 3, 1, 3, 1, 1, 3],
+    #         [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    #     ]
+    # ),
 }
-
+print(PATH / "bird")
+bird_motifs = {str(png) : png2motif(str(png)) for png in (PATH / "bird").glob("*.png")}
 
 def fg_square(w: int) -> Motif:
     """Return a w x w motif where every color is the foreground color"""
